e81bb58c9b67188fca79be29bf6a5256e956e701
AngeloArtur <ang.vianna@gmail.com>
2024-05-15 12:18:31 -0300

    remove: temporarily the password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,21 @@ import { MonitoringComponent } from './monitoring/monitoring.component';
 import { RegisterComponent } from './register/register.component';
 import { UserComponent } from './user/user.component';
 import { LogComponent } from './log/log.component';
+import { SnTestDialogComponent } from './shared/components/sn-test-dialog/sn-test-dialog.component';
 
 const routes: Routes = [
-  {path: '', component: AuthComponent},
-  {path: 'Monitoring', component: MonitoringComponent},
-  {path: 'Register', component: RegisterComponent},
-  {path: 'Register/:key', component: RegisterComponent }, 
-  {path: 'User', component: UserComponent},
-  {path: 'User/:Action/:key', component: UserComponent},
-  {path: 'Log/:key/:shift', component: LogComponent},
+  { path: '', component: AuthComponent },
+  { path: 'Monitoring', component: MonitoringComponent },
+  { path: 'Register', component: RegisterComponent },
+  { path: 'Register/:key', component: RegisterComponent },
+  { path: 'User', component: UserComponent },
+  { path: 'User/:Action/:key', component: UserComponent },
+  { path: 'Log/:key/:shift', component: LogComponent },
+  { path: 'Monitoring/SNTest', component: SnTestDialogComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,15 +18,21 @@ import { TextFieldModule } from '@angular/cdk/text-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatRadioModule} from '@angular/material/radio';
-import { MatIconModule} from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatIconModule } from '@angular/material/icon';
 import { ConfigDialogComponent } from './shared/components/config-dialog/config-dialog.component';
-import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogModule, MatDialogTitle} from '@angular/material/dialog';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 import { NewDataBaseDialogComponent } from './shared/components/new-data-base-dialog/new-data-base-dialog.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { PendingDialogComponent } from './shared/components/pending-dialog/pending-dialog.component';
 import { UserComponent } from './user/user.component';
-import { MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
 import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 import { HttpClientModule } from '@angular/common/http';
 import { LicenseDialogComponent } from './shared/components/license-dialog/license-dialog.component';
@@ -34,12 +40,20 @@ import { environment } from '../environments/environment';
 import { ConfigDropboxDialogComponent } from './shared/components/config-dropbox-dialog/config-dropbox-dialog.component';
 import { GenerateTokenDialogComponent } from './shared/components/generate-token-dialog/generate-token-dialog.component';
 import { LogComponent } from './log/log.component';
-import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatExpansionModule} from '@angular/material/expansion';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatListModule } from '@angular/material/list';
+import { MatSortModule } from '@angular/material/sort';
+import { MatCardModule } from '@angular/material/card';
+import { SnTestDialogComponent } from './shared/components/sn-test-dialog/sn-test-dialog.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { SnTestLogComponent } from './shared/components/sn-test-log/sn-test-log.component';
+import { SnTestListComponent } from './shared/components/sn-test-list/sn-test-list.component';
 @NgModule({
   declarations: [
     AuthComponent,
@@ -54,6 +68,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ConfigDropboxDialogComponent,
     GenerateTokenDialogComponent,
     LogComponent,
+    SnTestDialogComponent,
+    NavbarComponent,
+    SnTestLogComponent,
+    SnTestListComponent,
   ],
   imports: [
     BrowserModule,
@@ -68,7 +86,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatCheckboxModule,
     MatSelectModule,
-    TextFieldModule, 
+    TextFieldModule,
     MatButtonModule,
     MatAutocompleteModule,
     ReactiveFormsModule,
@@ -79,17 +97,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatDialogClose,
     MatDialogTitle,
     MatDialogContent,
+    MatSortModule,
     MatSnackBarModule,
     MatMenuModule,
+    MatListModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatCardModule,
     NgxMaskDirective,
     NgxMaskPipe,
     HttpClientModule,
     MatDatepickerModule,
     MatTooltipModule,
     MatExpansionModule,
+    CdkDropList,
+    CdkDrag,
+    MatListModule,
+    MatGridListModule,
   ],
   providers: [AuthService, provideNgxMask()],
   bootstrap: [AppComponent],
-
 })
-export class AppModule { }
+export class AppModule {}
